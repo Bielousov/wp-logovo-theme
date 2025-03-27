99a60b2a70c0faca1e80d3a07fb66a18ea2ce0fb
Anton Bielousov <anton.bielousov@gmail.com>
2025-03-26 17:27:02 -0700

    Monetization block on 404

--- 404.php
+++ 404.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The template for displaying the 404 template in the Twenty Twenty theme.
  *
@@ -12,39 +13,43 @@ get_header();
 
 <main id="site-content" role="main">
 
-	<div class="section-inner thin logovo-404">
+  <div class="section-inner thin logovo-404">
 
-		<h1 class="entry-title"><?php _e( 'Ошиблись адресом?', 'twentytwenty' ); ?></h1>
+    <h1 class="entry-title"><?php _e('Ошиблись адресом?', 'twentytwenty'); ?></h1>
 
-		<div class="intro-text"><p><?php _e( 'Такой страницы пока (или больше) нет, попробуйте найти интересующий вас пост:', 'twentytwenty' ); ?></p></div>
-		
-		<?php
-      get_search_form(
-        array(
-          'label' => __( '404 not found', 'twentytwenty' ),
-        )
-      );
-		?>
-	</div>
+    <div class="intro-text">
+      <p><?php _e('Такой страницы пока (или больше) нет, попробуйте найти интересующий вас пост:', 'twentytwenty'); ?></p>
+    </div>
+
+    <?php
+    get_search_form(
+      array(
+        'label' => __('404 not found', 'twentytwenty'),
+      )
+    );
+    ?>
+  </div>
 
 
   <div class="post-inner thin">
     <div class="entry-content">
-      <?php 
-        $id=18089; 
-        $post = get_post($id); 
-        $content = apply_filters('the_content', $post->post_content); 
-        echo $content;  
+      <?php
+      $id = 18089;
+      $post = get_post($id);
+      $content = apply_filters('the_content', $post->post_content);
+      echo $content;
       ?>
-		</div>
-	</div>
-		
+    </div>
+  </div>
 
-	</div><!-- .section-inner -->
+
+  <div class="section-inner">
+    <?php echo the_ad(22179); ?>
+  </div><!-- .section-inner -->
 
 </main><!-- #site-content -->
 
-<?php get_template_part( 'template-parts/footer-menus-widgets' ); ?>
+<?php get_template_part('template-parts/footer-menus-widgets'); ?>
 
 <?php
 get_footer();
