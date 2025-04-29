afc871e8737b0bcdfe575c3ae137f7ffcde490f4
Anton Bielousov <anton.bielousov@gmail.com>
2025-04-29 13:14:55 -0700

    Style no search results

--- index.php
+++ index.php
@@ -25,53 +25,9 @@ $archive_subtitle = '';
 
 	<?php
 	if (is_search()) {
-		global $wp_query;
-
-		$archive_title = sprintf(
-			'%1$s %2$s',
-			'<span class="color-accent">' . __('Результаты поиска:', 'twentytwenty') . '</span>',
-			'&ldquo;' . get_search_query() . '&rdquo;'
-		);
-
-		if ($wp_query->found_posts) {
-			$archive_subtitle = sprintf(
-				/* translators: %s: Number of search results */
-				_n(
-					'Найден всего один пост, зато какой!',
-					'Найдено %s постов',
-					$wp_query->found_posts,
-					'twentytwenty'
-				),
-				number_format_i18n($wp_query->found_posts)
-			);
-		} else {
-			$archive_subtitle = __('Я искал, искал, но ничего не нашел. Попробуйте иначе сформулировать свой запрос.', 'twentytwenty');
-		}
+		get_template_part('template-parts/search-results');
 	} elseif (! is_home()) {
-		$archive_title    = get_the_archive_title();
-		$archive_subtitle = get_the_archive_description();
-	}
-
-	if ($archive_title || $archive_subtitle) {
-	?>
-
-		<header class="archive-header has-text-align-center header-footer-group">
-
-			<div class="archive-header-inner section-inner medium">
-
-				<?php if ($archive_title) { ?>
-					<h1 class="archive-title"><?php echo wp_kses_post($archive_title); ?></h1>
-				<?php } ?>
-
-				<?php if ($archive_subtitle) { ?>
-					<div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post(wpautop($archive_subtitle)); ?></div>
-				<?php } ?>
-
-			</div><!-- .archive-header-inner -->
-
-		</header><!-- .archive-header -->
-
-	<?php
+		get_template_part('template-parts/archive-header');
 	}
 
 	if (have_posts()) {
@@ -91,22 +47,6 @@ $archive_subtitle = '';
 
 			get_template_part('template-parts/content', get_post_type());
 		}
-	} elseif (is_search()) {
-	?>
-
-		<div class="no-search-results-form section-inner thin">
-
-			<?php
-			get_search_form(
-				array(
-					'label' => __('search again', 'twentytwenty'),
-				)
-			);
-			?>
-
-		</div><!-- .no-search-results -->
-
-	<?php
 	}
 	?>
 
