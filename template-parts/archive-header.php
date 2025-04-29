f74aa32d4145c9b69e4a7bd6e74b2c149f0a9a76
Anton Bielousov <anton.bielousov@gmail.com>
2025-04-29 13:18:25 -0700

    Style no search results

---
+++ template-parts/archive-header.php
@@ -0,0 +1,21 @@
+<?php
+if (is_archive() && !is_search() && !is_home()) {
+    $archive_title    = get_the_archive_title();
+    $archive_subtitle = get_the_archive_description();
+}
+
+if ($archive_title || $archive_subtitle) {
+?>
+    <header class="archive-header has-text-align-center header-footer-group">
+        <div class="section-inner medium">
+            <?php if ($archive_title) { ?>
+                <h1 class="archive-title"><?php echo wp_kses_post($archive_title); ?></h1>
+            <?php } ?>
+            <?php if ($archive_subtitle) { ?>
+                <div class="archive-subtitle section-inner thin max-percentage intro-text"><?php echo wp_kses_post(wpautop($archive_subtitle)); ?></div>
+            <?php } ?>
+        </div>
+    </header><!-- .archive-header -->
+<?php
+}
+?>
---
+++ template-parts/search-results.php
@@ -0,0 +1,72 @@
+<?php
+
+/**
+ * Search results page header
+ */
+
+//  global $wp_query;
+
+$search_title = sprintf(
+    '%1$s %2$s',
+    '<span class="color-accent">' . __('Где тут у вас', 'twentytwenty') . '</span>',
+    '<span class="logovo-search-result-term">' . get_search_query() . '</span>?',
+);
+
+if ($wp_query->found_posts) {
+    $search_subtitle = sprintf(
+        /* translators: %s: Number of search results */
+        _n(
+            'Нашел! Всего один пост, зато какой!',
+            'Этого добра у нас навалом. Найдено %s постов',
+            $wp_query->found_posts,
+            'twentytwenty'
+        ),
+        number_format_i18n($wp_query->found_posts)
+    );
+} else {
+    $search_subtitle = __('Ну вот, опять потерялись!', 'twentytwenty');
+}
+
+if (is_search()) {
+?>
+
+    <header class="has-text-align-center logovo-search-results has-text-align-center">
+        <div class="section-inner medium">
+            <?php
+            if (!$wp_query->found_posts) {
+            ?>
+                <img src="/wp-content/themes/wp-logovo-theme/assets/images/search-no-results.webp" alt="No search results" class="logovo-search-results-image" />
+            <?php
+            }
+            ?>
+
+            <h1 class="logovo-search-results-title ">
+                <?php echo wp_kses_post($search_title); ?>
+            </h1>
+
+            <p class="logovo-search-results-subtitle thin has-text-align-center">
+                <?php echo wp_kses_post($search_subtitle); ?>
+            </p>
+        </div>
+    </header>
+
+
+    <?php
+    if (!$wp_query->found_posts) {
+    ?>
+        <div class="logovo-search-no-results-form section-inner thin">
+            <?php
+            get_search_form(
+                array(
+                    'aria_label' => __('search again', 'twentytwenty'),
+                )
+            );
+            ?>
+            <p class="logovo-search-no-results-map has-text-align-center">Попробуйте <a href="/travel">свериться с картой</a> 🗺️</p>
+        </div>
+    <?php
+    }
+    ?>
+<?php
+}
+?>
