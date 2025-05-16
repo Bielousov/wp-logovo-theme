adf3c01618ac7eb90980025cbab2a20171842696
Anton Bielousov <anton.bielousov@gmail.com>
2025-05-16 14:06:24 -0700

    Style parental advisory

--- template-parts/parental-advisory.php
+++ template-parts/parental-advisory.php
@@ -1,4 +1,4 @@
-<aside data-rating="G" class="logovo-explicit-content">
+<aside data-rating="G" class="logovo-explicit-content has-text-align-center">
     <img
         loading="lazy"
         decoding="async"
