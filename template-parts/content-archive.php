96eea971c4d1f6ff6089584b39a5eaaca3742236
Anton Bielousov <anton.bielousov@airspacelink.com>
2025-09-04 11:52:33 -0700

    Support short format for ad posts

--- template-parts/content-archive.php
+++ template-parts/content-archive.php
@@ -14,34 +14,44 @@
 ?>
 
 <?php
-get_template_part( 'template-parts/entry-header' );
-get_template_part( 'template-parts/featured-image' );
+  $isShort = get_post_meta(get_the_ID(), 'ad-post', true);
+
+  get_template_part( 'template-parts/entry-header' );
+
+  if (!$isShort) {
+    get_template_part( 'template-parts/featured-image' );
 ?>
 
-<div class="post-inner">
+  <div class="post-inner">
+    <div class="entry-content">
+      <div class="entry-excerpt">
+      <?php the_excerpt( __( 'Читать дальше', 'twentytwenty' ) ); ?>
+      </div>
+    </div><!-- .entry-content -->
+  </div><!-- .post-inner -->
 
-<div class="entry-content">
-  <div class="entry-excerpt">
-	<?php the_excerpt( __( 'Читать дальше', 'twentytwenty' ) ); ?>
+  <div class="section-inner">
+    <?php
+      wp_link_pages(
+      array(
+      'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Страница', 'twentytwenty' ) . '"><span class="label">' . __( 'Страница:', 'twentytwenty' ) . '</span>',
+      'after'       => '</nav>',
+      'link_before' => '<span class="page-number">',
+      'link_after'  => '</span>',
+      )
+      );
+
+      edit_post_link();
+
+      // Single bottom post meta.
+      twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
+    ?>
+  </div><!-- .section-inner -->
+
+<?php } else { ?>
+
+  <div class="section-inner">
+    <?php edit_post_link(); ?>
   </div>
-</div><!-- .entry-content -->
 
-</div><!-- .post-inner -->
-
-<div class="section-inner">
-<?php
-wp_link_pages(
-array(
-'before'      => '<nav class="post-nav-links bg-light-background" aria-label="' . esc_attr__( 'Страница', 'twentytwenty' ) . '"><span class="label">' . __( 'Страница:', 'twentytwenty' ) . '</span>',
-'after'       => '</nav>',
-'link_before' => '<span class="page-number">',
-'link_after'  => '</span>',
-)
-);
-
-edit_post_link();
-
-// Single bottom post meta.
-twentytwenty_the_post_meta( get_the_ID(), 'single-bottom' );
-?>
-</div><!-- .section-inner -->
+<?php } ?>
--- template-parts/entry-content.php
+++ template-parts/entry-content.php
@@ -1,20 +1,20 @@
 <?php
-$postClass = '';
-if (!is_page_template('templates/template-full-width.php')) {
+  $postClass = '';
+  if (!is_page_template('templates/template-full-width.php')) {
     if (is_page_template('templates/template-full-headless.php') || is_page_template('templates/template-full-screen.php')) {
         $postClass = 'full';
     } else {
         $postClass = 'medium';
     }
-}
+  }
 
-if (!is_page_template('templates/template-full-headless.php')) {
+  if (!is_page_template('templates/template-full-headless.php')) {
     get_template_part('template-parts/entry-header');
-}
+  }
 ?>
 
 <div class="post-inner <?php echo $postClass; ?>" id="post-inner">
-    <div class="entry-content">
-        <?php the_content(); ?>
-    </div>
+  <div class="entry-content">
+    <?php the_content(); ?>
+  </div>
 </div>
