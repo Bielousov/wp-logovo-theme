d2ce7f9ee3b373811daafbebc8ba5177d3715e51
Anton Bielousov <anton.bielousov@gmail.com>
2026-02-17 12:32:30 -0800

    Apply custom shareaholic thumbnail size

--- functions.php
+++ functions.php
@@ -763,3 +763,8 @@ remove_action('wp_print_styles', 'print_emoji_styles');
 
 remove_action('admin_print_scripts', 'print_emoji_detection_script');
 remove_action('admin_print_styles', 'print_emoji_styles');
+
+/* Update Shareaholic thumbnail size to match custom size */
+add_filter('shareaholic_related_thumb_size', function($size){
+    return 'shareaholic-thumb';
+});
